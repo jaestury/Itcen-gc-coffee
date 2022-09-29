3f69739ba8305c15acfa3d7b386fced605160632
KIM JaeHyeon <rla_wogus@naver.com>
2022-09-29 11:38:04 +0900

    set OrderRepo

--- gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
+++ gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -7,9 +7,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.nio.ByteBuffer;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.*;
 
 import static com.example.gccoffee.JdbcUtils.toUUID;
