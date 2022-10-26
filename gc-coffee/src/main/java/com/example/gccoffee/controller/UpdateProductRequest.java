5d0d3c38ccb1ce49056a7df75ad626e597dd4926
KIM JaeHyeon <rla_wogus@naver.com>
2022-10-26 20:59:17 +0900

    feat 상품조회

--- gc-coffee/src/main/java/com/example/gccoffee/controller/UpdateProductRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.gccoffee.controller;
-
-import com.example.gccoffee.model.Category;
-
-public record UpdateProductRequest(String productName, Category category, long price, String description) {
-}
