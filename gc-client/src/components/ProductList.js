3d1c335a23b4d5027501f4c68363dd7f1caf7873
KIM JaeHyeon <rla_wogus@naver.com>
2022-10-26 21:38:19 +0900

    fix3

--- gc-client/src/components/ProductList.js
+++ gc-client/src/components/ProductList.js
@@ -1,20 +1,18 @@
 import React from "react";
-import { Product } from "./Product";
+import {Product} from "./Product";
 
-export function ProductList({ products = [], onAddClick }) {
-  return (
-    <React.Fragment>
-      <h5 className="flex-grow-0">
-        <b>상품 목록</b>
-      </h5>
-      <ul className="list-group products">
-        {products.map((v) => (
-          <li key={v.productId} className="list-group-item d-flex mt-3">
-            {/* 프로퍼티같으면 ...표현(스프레드연산자) */}
-            <Product {...v} onAddClick={onAddClick} />
-          </li>
-        ))}
-      </ul>
-    </React.Fragment>
-  );
+export function ProductList({products = [], onAddClick, onSubstractClick}) {
+    return (
+        <React.Fragment>
+            <h5 className="flex-grow-0"><b>상품 목록</b></h5>
+            <ul className="list-group products">
+                {products.map(v =>
+                    <li key={v.productId} className="list-group-item d-flex mt-3">
+                        <Product {...v} onAddClick={onAddClick}  onSubstractClick={onSubstractClick} />
+                    </li>
+                )}
+
+            </ul>
+        </React.Fragment>
+    )
 }
